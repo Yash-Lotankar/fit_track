0a757b9295a400e050b00e61f528c27f535ab5dd
Yash <yashlotankar777@gmail.com>
2026-04-21 07:16:04 +0530

    OK

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/workouts', workoutRoutes);
 
 // Fallback to index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
